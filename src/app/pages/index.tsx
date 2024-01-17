3bef4d1456d0f0eaac9d2f502757d50e503a86ae
housuwen <housuwen@sugo.io>
2024-01-17 14:02:36 +0800

    chore: 增加网站工具

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import React from 'react'
 import './index.scss'
 import Input from '@/app/components/BasicInput'
@@ -59,6 +60,7 @@ const DocsList = () => {
         </div>
       </section>
       <Analytics />
+      <SpeedInsights />
     </main>
   )
 }
